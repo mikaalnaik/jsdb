2d6e674b70a5fe55b66337f1d19b8d9750ec5247
Mikaal Naik <matthewnaik@gmail.com>
2018-05-01 21:59:54 +0000

    add milestones foreign key

---
+++ migrations/20180501214919_setup.js
@@ -0,0 +1,15 @@
+exports.up = function(knex, Promise) {
+  return Promise.all([
+    knex.schema.table('milestones', function(table){
+      table.foreign("id").references("famous_people.id");
+    })
+  ])
+};
+
+exports.down = function(knex, Promise) {
+  return Promise.all([
+    knex.schema.table('milestones', function(table){
+      table.dropColumn('id');
+    })
+  ])
+};
